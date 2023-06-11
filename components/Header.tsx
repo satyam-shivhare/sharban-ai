35319c38282729b86b7c62a24a9c2488ea32af6f
Satyam Shivhare <satyamshivhare@Satyams-MacBook-Air.local>
2023-06-11 22:44:36 +0530

    Initial Commit: Creating a web based project after sometime and man I really struggled to get things up and running :)

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const links : String[] = ['Products' , 'Pricing', 'Support', 'Login']
+
+export default function Header() : React.JSX.Element {
+    return(
+    <nav className=" bg-white flex justify-between items-center  p-4 border-b-2  shadow-sm ">
+      {/* logo */}
+      hhhf
+    </nav>
+    )
+}
