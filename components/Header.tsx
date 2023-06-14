077db163609044b3494f836a46778f644cd3801c
Satyam Shivhare <satyamshivhare@Satyams-MacBook-Air.local>
2023-06-14 23:38:06 +0530

    thinking if I should keep using the tailwind or move to Material UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,45 @@
 import React from "react"
 
-const links : String[] = ['Products' , 'Pricing', 'Support', 'Login']
+const links: String[] = ['Products', 'Pricing', 'Support', 'Login']
 
-export default function Header() : React.JSX.Element {
-    return(
-    <nav className=" bg-white flex justify-between items-center  p-4 border-b-2  shadow-sm ">
+export default function Header(): React.JSX.Element {
+  return (
+    <nav className=" bg-white flex justify-between items-center px-20 w-full py-4  h-20  shadow-sm ">
       {/* logo */}
-      hhhf
+      <span className=" font-bold text-2xl ">Sharban.ai</span>
+      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              Showcase
+            </a>
+          </li>
+          <li>
+            <button id="sharbanResourcesDropdown" data-dropdown-toggle="sharbanResourcesList" data-dropdown-placement="right-start" type="button" className="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Resources <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></button>
+            <div id="sharbanResourcesList" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+              <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Overview</a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">My downloads</a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Billing</a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Rewards</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
-    )
+  )
 }
